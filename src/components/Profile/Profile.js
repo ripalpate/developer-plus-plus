68fc63edcdc815bcc10b62f3f648e472a0017ba2
Ripal Patel <ripal212007@gmail.com>
2018-12-15 13:55:48 -0600

    headings are printing

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Profile.scss';
+
+class Profile extends React.Component {
+  render() {
+    return (
+      <div className="Profile col">
+        <h2>Profile</h2>
+      </div>
+    );
+  }
+}
+
+export default Profile;
