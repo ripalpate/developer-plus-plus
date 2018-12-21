dc58d85011e2880b29265d91945cbacc7686fe6c
Ripal Patel <ripal212007@gmail.com>
2018-12-20 22:02:26 -0600

    delete button displays for both

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,14 +1,31 @@
 import React from 'react';
 
 import './Blogs.scss';
+import authRequests from '../../helpers/data/authRequests';
 
 class Blogs extends React.Component {
   render() {
     const { blog } = this.props;
+    const uid = authRequests.getCurrentUid();
+    const makeButtons = () => {
+      if (blog.uid === uid) {
+        return (
+          <div className="col-2 button-blog">
+            <span className="col">
+              <button className="btn btn-default">
+                <i className="fas fa-trash-alt"></i>
+              </button>
+            </span>
+          </div>
+        );
+      }
+      return <span className="col-2"></span>;
+    };
     return (
       <li className="blog-item row">
         <span className="col-4">{blog.name}</span>
         <a href={blog.url} className="col-4">{blog.url}</a>
+        {makeButtons()}
       </li>
     );
   }
--- src/components/Podcasts/Podcasts.js
+++ src/components/Podcasts/Podcasts.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import './Podcasts.scss';
 import trackerShape from '../../helpers/propz/trackerShape';
+import authRequests from '../../helpers/data/authRequests';
 
 class Podcasts extends React.Component {
   static propTypes = {
@@ -10,10 +11,26 @@ class Podcasts extends React.Component {
 
   render() {
     const { podcast } = this.props;
+    const uid = authRequests.getCurrentUid();
+    const makeButtons = () => {
+      if (podcast.uid === uid) {
+        return (
+          <div className="col-2 button-podcast">
+            <span className="col">
+              <button className="btn btn-default">
+                <i className="fas fa-trash-alt"></i>
+              </button>
+            </span>
+          </div>
+        );
+      }
+      return <span className="col-2"></span>;
+    };
     return (
       <li className="podcast-item row">
         <span className="col-4">{podcast.name}</span>
         <a href={podcast.url} className="col-4">{podcast.url}</a>
+        {makeButtons()}
     </li>
     );
   }
