496ab678d9b201cf1e22affa37b3da816c060862
Ripal Patel <ripal212007@gmail.com>
2018-12-21 22:02:46 -0600

    form displaying

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -6,6 +6,31 @@ class Form extends React.Component {
     return (
       <div className="Form col">
         <h2>Form</h2>
+        <form className="">
+          <label className="sr-only" for="name">Name</label>
+          <input type="text" className="form-control mb-2 mr-sm-2 mb-sm-0" id="name" placeholder="Learn HTML"/>
+          <div className="form-check">
+            <label className="form-check-label">
+            <input className="form-check-input" type="checkbox"/> Tutorial
+            </label>
+          </div>
+          <div className="form-check">
+            <label className="form-check-label">
+            <input className="form-check-input" type="checkbox"/> Resources
+            </label>
+          </div>
+          <div className="form-check">
+            <label className="form-check-label">
+            <input className="form-check-input" type="checkbox"/> Podcasts
+            </label>
+          </div>
+          <div className="form-check">
+            <label className="form-check-label">
+            <input className="form-check-input" type="checkbox"/> Blogs
+            </label>
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
       </div>
     );
   }
