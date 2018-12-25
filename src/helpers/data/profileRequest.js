8c8caf016e71b593c03751a35c57e779d3ef574b
Ripal Patel <ripal212007@gmail.com>
2018-12-24 21:34:30 -0600

    getting commits data

--- src/helpers/data/profileRequest.js
+++ src/helpers/data/profileRequest.js
@@ -13,6 +13,21 @@ const getRequest = githubUsername => new Promise((resolve, reject) => {
     .catch(err => reject(err));
 });
 
+const getUserCommit = githubUsername => new Promise((resolve, reject) => {
+  axios
+    .get(`https://api.github.com/users/${githubUsername}/events/public`)
+    .then((result) => {
+      // console.log(result.data);
+      // let commitsCount = 0;
+      let pushEvent = result.data.filter(event => event.type === 'PushEvent');
+      pushEvent = pushEvent.slice(0, 5);
+      // commitsCount += pushEvent.payload.commits;
+      console.log(pushEvent);
+    })
+    .catch(err => reject(err));
+});
+
 export default {
   getRequest,
+  getUserCommit,
 };
